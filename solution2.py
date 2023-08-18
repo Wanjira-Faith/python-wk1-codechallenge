66ae369fff9941660ff60df6a2c1535714ea66ed
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-18 14:59:30 +0300

    tested solution2

--- solution2.py
+++ solution2.py
@@ -4,12 +4,17 @@ def two_exact_positive_numbers(a,b,c):
  if a > 0:
   positive_count +=1
 
-  if b > 0:
+ if b > 0:
    positive_count +=1
 
-   if c > 0:
+ if c > 0:
     positive_count +=1
 
-    return positive_count == 2
+ return positive_count == 2
+   
+print(two_exact_positive_numbers(2,4,-3))
+print(two_exact_positive_numbers(-4,6,0))
+print(two_exact_positive_numbers(-14,-3,-4))
+
  
 
