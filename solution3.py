60e932c69f39de32e4de83b7adefed4f926ff1b0
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-18 15:42:21 +0300

    returns max highes value

--- solution3.py
+++ solution3.py
@@ -8,7 +8,9 @@ def solve(x):
            current_value += ord(char) -ord('a') + 1
         else:
             max_value = max(max_value,current_value)
-            current_value = 0   
+            current_value = 0
+
+            return max(max_value,current_value)   
 
 
 
