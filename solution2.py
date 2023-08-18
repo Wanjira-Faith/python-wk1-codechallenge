988cc4ad923aaa7ec664b20e41846b3c9a2fd794
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-18 14:47:00 +0300

    code checks if a,band c are greater than 0,meaning they are positive so positive coutn is incremented by 1

--- solution2.py
+++ solution2.py
@@ -1,5 +1,14 @@
 def two_exact_positive_numbers(a,b,c):
  positive_count = 0
 
+ if a > 0:
+  positive_count +=1
+
+  if b > 0:
+   positive_count +=1
+
+   if c > 0:
+    positive_count +=1
+    
  
 
