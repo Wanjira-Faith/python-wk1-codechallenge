27fb518af24432d427926b64e63d3f6e984a0fd5
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-18 14:15:19 +0300

    format hr and minute as a four digit string,returns time in 24-hr format

--- solution1.py
+++ solution1.py
@@ -3,5 +3,9 @@ def convert_to_24_hour(hour,minute,period):
         hour += 12
     elif period == "am" and hour == 12:
         hour = 0
-        
-     
+
+    time_to_24_hour = f"{hour:02d}{minute:02d}"
+    return time_to_24_hour
+
+
+
