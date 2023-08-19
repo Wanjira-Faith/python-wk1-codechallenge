6e580b592bf1de88c503167862934d3326fde9ed
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-08-19 15:18:40 +0300

    removed the print()

--- solution2.py
+++ solution2.py
@@ -12,9 +12,7 @@ def two_exact_positive_numbers(a,b,c):
 
  return positive_count == 2
    
-print(two_exact_positive_numbers(2,4,-3))
-print(two_exact_positive_numbers(-4,6,0))
-print(two_exact_positive_numbers(-14,-3,-4))
+
 
  
 
